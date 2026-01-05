ec54928439a272c525fb159744d1350ffacdd23e
Dashinnnn <yagodamaratas28@gmail.com>
2026-01-05 21:49:38 +0800

    update: finished converting images of about into UI

--- frontend/src/app/(features)/home/about/About.tsx
+++ frontend/src/app/(features)/home/about/About.tsx
@@ -3,6 +3,9 @@ import "./aboutMq.css"
 
 import Image from "next/image";
 import aboutMain from "../../../../../public/_assets/photo/aboutMain.png";
+import concorde from "../../../../../public/_assets/images/concorde-image.png"
+
+import { IconUserCircle, IconCalendarCheck, IconNotebook, IconBell } from "@tabler/icons-react";
 
 const About = () => {
   return (
@@ -10,11 +13,106 @@ const About = () => {
     <div className="homeabout-box">
     <div className="homeabout-box-in">
       <div className="homeabout-ui">
-        <Image
-            src={aboutMain}
-            alt="ui"
-        />
+        <div className="homeabout-ui-left">
+          <Image
+            src={concorde}
+            alt="concorde logo"
+            className="concorde-logo"
+          />
 
+          <div className="statistic-container">
+            <div className="statistic-container-top">
+              <div className="statistic">
+                <div className="graph"></div>
+                <div className="statistics-info">
+                  <div className="statistics-percentage"><h1>- 40%</h1></div>
+                  <div className="statistics-category">Time Delays</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="statistic-container-bottom">
+              <div className="statistic">
+                <div className="graph"></div>
+                <div className="statistics-info">
+                  <div className="statistics-percentage"><h1>+ 30%</h1></div>
+                  <div className="statistics-category">Opearational Efficiency</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="homeAbout-ui-right">
+          <div className="guide-container">
+            <div className="guide">
+              <div className="guide-header">
+                <p>How it works</p>
+              </div>
+
+              <div className="guide-nav-icons">
+                <div className="nav-white">
+                  <div className="icon-container-white">
+                    <IconUserCircle
+                      strokeWidth={1}
+                      size={45}
+                      color="var(--primary-color)"  
+                      fill="var(--white-color)"
+                    />
+                  </div>
+                  <div className="nav-title">
+                    <p>User</p>
+                    <p>Registration</p>
+                  </div>
+                </div>
+
+                <div className="nav-white">
+                  <div className="icon-container-white">
+                    <IconCalendarCheck
+                      strokeWidth={1}
+                      size={45}
+                      color="var(--primary-color)"  
+                      fill="var(--white-color)"
+                    />
+                  </div>
+                  <div className="nav-title">
+                    <p>Easy</p>
+                    <p>Scheduling</p>
+                  </div>
+                </div>
+
+                <div className="nav-primary">
+                  <div className="icon-container-primary">
+                    <IconNotebook
+                      strokeWidth={1}
+                      size={45}
+                      color="var(--white-color)"  
+                      fill="var(--primary-color)"
+                    />
+                  </div>
+                  <div className="nav-title">
+                    <p>Certification</p>
+                    <p>Tracking</p>
+                  </div>
+                </div>
+                <div className="nav-white">
+                  <div className="icon-container-white">
+                    <IconBell
+                      strokeWidth={1}
+                      size={45}
+                      color="var(--primary-color)"  
+                      fill="var(--white-color)"
+                    />
+                  </div>
+                  <div className="nav-title">
+                    <p>Real-Time</p>
+                    <p>Notification</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="homeabout-text">
         {/* <p className="homeabout-text-light">a little bit</p> */}
